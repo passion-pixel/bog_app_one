beda2dd5b5f909effff6bd10d45885c21b5fef7d
Natalie Contreras <natalie.isabel.contreras.a@gmail.com>
2018-02-22 12:28:19 -0800

    Forms

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -1,6 +1,19 @@
 class CreaturesController < ApplicationController
   def index
   @creatures = Creature.all
+  end
 
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+
+    creature = Creature.new(creature_params)
+
+    if creature.save
+      redirect_to creatures_path
+    end
   end
 end
